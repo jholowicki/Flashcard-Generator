0019967175e5d8b8cea0695cecbd5f14a513c5cc
Joy Holowicki <joyholowicki@gmail.com>
2016-12-30 23:39:15 -0600

    Added Dependencies

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var inquirer = require("inquirer");
 var fs = require('fs');
 
-//Constructor function for a basic flashcard object
+//Constructor function for a basic flashcard object.
 BasicFlashcard = function(frontArg, backArg) {
     this.front = frontArg,
         this.back = backArg,
@@ -11,7 +11,7 @@ BasicFlashcard = function(frontArg, backArg) {
         },
         this.showBack = function() {
             console.log(this.back);
-        }
+        };
 };
 
 //Constructor function for a cloze-deleted flashcard object.
@@ -23,94 +23,92 @@ ClozeFlashcard = function(textArg, clozeArg) {
         },
         this.showCloze = function() {
             console.log(this.clozeArg);
-        }
+        };
 };
 
 //Flag to signify if first or multiple card to be generated.
 var cardNumber = 0;
-//Array to contain user-generated flashcards.
-var userFlashcards = [];
 
 //Function to allow user to select basic or cloze-deleted flashcard to create.
 var selectCardType = function() {
-    if (cardNumber === 0) {
-        console.log("Welcome! This program creates basic or cloze-deleted flashcards.");
-    }
+        if (cardNumber === 0) {
+            console.log("Welcome! This program creates basic or cloze-deleted flashcards.");
+        }
 
-    inquirer.prompt([{
+        inquirer.prompt([{
             type: 'list',
             name: 'type',
             message: 'What type of flashcard do you want to create?',
             choices: ['basic', 'close-deleted']
+        }]).then(function(answer) {
+            if (answer.type === 'basic') {
+                basicCardInquiry();
+            } else {
+                clozeCardInquiry();
             }
-        }
-    }]).then(function(answer) {
-    if (answer.type === 'basic') {
-        basicCardInquiry();
-    } else {
-        clozeCardInquiry();
-    };
-});
+        });
 
-//Function to take in user card information and create a basic flashcard. Writes to file.
+        //Function to take in user card information and create a basic flashcard. Writes to file.
 
-var basicCardInquiry = function(){
-	inquirer.prompt([{
-            name: "front",
-            message: "What is on the front of the card?"
-            },{
-            name: "back",
-            message: "What is on the back of the card?",
-            {
-            type: "confirm",
-            name: "save",
-            message: "Would you like to save flashcard to file?"
-        }
-	}]).then(function(answers)){
-		if(answers.save){
-			var newFlashcard = new BasicFlashcard(answers.front, answers.back);
-			fs.writeFile('cards.txt', JSON.stringify(newFlashcard), 'utf-8');
-			console.log("New Flashcard created and saved! /n Front:" + answers.front + "/n Back:"+ answers.back);
-			cardNumber++;
-			selectCardType();
-		} else
-		cardNumber++;
-		selectCardType();
-	}
-	}
-};
+        var basicCardInquiry = function() {
+            inquirer.prompt([{
+                name: "front",
+                message: "What is on the front of the card?"
+            }, {
+                name: "back",
+                message: "What is on the back of the card?",
+            }, {
+                type: "confirm",
+                name: "save",
+                message: "Would you like to save flashcard to file?"
+            }]).then(function(answers) {
+                if (answers.save) {
+                    var newFlashcard = new BasicFlashcard(answers.front, answers.back);
+                    fs.writeFile('cards.txt', JSON.stringify(newFlashcard), 'utf-8');
+                    console.log("New Flashcard created and saved! /n Front:" + answers.front + "/n Back:" + answers.back);
+                    cardNumber++;
+                    selectCardType();
+                } else {
+                    cardNumber++;
+                    selectCardType();
+                };
 
-//Function to take in user card information and create a cloze-deleted flashcard. Writes to file.
+            });
+        };
 
-var clozeCardInquiry = function(){
-	inquirer.prompt([{
-            name: "text",
-            message: "What is the full text of the card?",
-            validate: function(value) {
-                if (isNaN(value) === true) {
-                    return true;
+        //Function to take in user card information and create a cloze-deleted flashcard. Writes to file.
+
+        var clozeCardInquiry = function() {
+            inquirer.prompt([{
+                name: "text",
+                message: "What is the full text of the card?",
+                validate: function(value) {
+                    if (isNaN(value) === true) {
+                        return true;
+                    }
+                    return false;
                 }
-                return false;
-            },{
-            name: "cloze",
-            message: "What is on the cloze-deleted side of the card?"
-            },{
-            type: "confirm",
-            name: "save",
-            message: "Would you like to save flashcard to file?"
-        }
-	}]).then(function(answers)){
-		if(answers.save){
-			var newFlashcard = new ClozeFlashcard(answers.text, answers.cloze);
-			fs.writeFile('cards.txt', JSON.stringify(newFlashcard), 'utf-8');
-			console.log("New Flashcard created and saved! /n text:" + answers.text + "/n with deletion:"+ answers.cloze);
-			cardNumber++;
-			selectCardType();
-		} else
-		cardNumber++;
-		selectCardType();
-	}
-};
+            }, {
+                name: "cloze",
+                message: "What is on the cloze-deleted side of the card?"
+            }, {
+                type: "confirm",
+                name: "save",
+                message: "Would you like to save flashcard to file?"
+            }]).then(function(answers) {
+                if (answers.save) {
+                    var newFlashcard = new ClozeFlashcard(answers.text, answers.cloze);
+                    fs.writeFile('cards.txt', JSON.stringify(newFlashcard), 'utf-8');
+                    console.log("New Flashcard created and saved! /n text:" + answers.text + "/n with deletion:" + answers.cloze);
+                    cardNumber++;
+                    selectCardType();
+                } else {
+                    cardNumber++;
+                    selectCardType();
+                };
 
-//Function to start the app.
-selectCardType();
+            })
+        };
+
+        //Function to start the app.
+        selectCardType();
